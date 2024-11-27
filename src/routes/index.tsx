51d4ce5c3dca1a58b31d9f604fd677d3c4603b0e
hex <hex@cowboy.dev>
2024-11-26 22:10:08 -0700

    fixes to be compatible with all browsers

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,21 +2,38 @@ import { HeroSvg } from "../assets/hero.svg";
 import { Border } from "../components/border";
 
 export default function Home() {
+  const isSafari = () => {
+    return (
+      /^((?!chrome|android).)*safari/i.test(navigator.userAgent) &&
+      !/CriOS/i.test(navigator.userAgent)
+    );
+  };
+
   return (
-    <div className="relative h-screen w-screen overflow-hidden">
+    <div className="relative h-dvh w-screen overflow-hidden">
       <Border />
 
-      <main className="absolute inset-0 overflow-y-auto gradient-overlay">
+      <main className="gradient-overlay absolute inset-0 overflow-y-auto">
         <div className="flex w-full flex-col items-center">
-          <div className="flex h-[70dvh] w-3/4 items-center justify-center md:w-1/2">
+          <div className="mt-20 flex h-[60dvh] w-3/4 items-center justify-center md:w-1/2">
             <HeroSvg />
           </div>
           <div className="flex w-full items-center justify-center p-8">
-            <img
-              src="/mountains.svg"
-              className="h-full w-full"
-              alt="Mountain illustration with cowboys roaming the landscape"
-            />
+            {isSafari() ? (
+              <object
+                type="image/svg+xml"
+                data="/mountains.svg"
+                className="h-full w-full"
+                aria-label="Mountain illustration with cowboys roaming the landscape"
+                role="img"
+              />
+            ) : (
+              <img
+                src="/mountains.svg"
+                className="h-full w-full"
+                alt="Mountain illustration with cowboys roaming the landscape"
+              />
+            )}
           </div>
         </div>
       </main>
